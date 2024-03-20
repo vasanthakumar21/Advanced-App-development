0153d443233b08472161bbcce50648884ebb7ea8
vasantha kumar.S <60564481+vasanthakumar21@users.noreply.github.com>
2024-03-20 21:54:07 +0530

    Add files via upload

---
+++ Backend/src/test/java/com/example/boathouse/BoathouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boathouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoathouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
